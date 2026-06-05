6984636b1aac55c405356d2c81b03b0bdeba066d
GreeshmaVG <greeshmavgowda925@gmail.com>
2026-06-05 18:11:11 +0530

    Revert - keep index.html; add post-build rename script

--- vite.config.js
+++ vite.config.js
@@ -5,6 +5,9 @@ import react from '@vitejs/plugin-react'
 export default defineConfig({
   plugins: [react()],
   base: '/swipesplit/',
+  build: {
+    outDir: 'dist',
+  },
   optimizeDeps: {
     exclude: ['@ffmpeg/ffmpeg', '@ffmpeg/util'],
   },
